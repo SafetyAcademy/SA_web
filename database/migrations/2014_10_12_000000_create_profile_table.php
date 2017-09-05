999e175ed3e6c490b102968d19cad0db798f5129
Vladislav Tomin <vlad@tomin.pro>
2017-09-05 16:25:18 +0400

    Created Profile migration

---
+++ database/migrations/2014_10_12_000000_create_profile_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('contact_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('street');
+            $table->string('postcode');
+            $table->string('city');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('profile');
+    }
+}
