2e80adcd5be5c6eabf094fa8887f162bbf42a051
Vladislav Tomin <vlad@tomin.pro>
2017-09-14 14:12:36 +0400

    Added projects_access table migrate

---
+++ database/migrations/2014_10_12_000000_create_projects_access_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsAccessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects_access', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->string('project_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects_access');
+    }
+}
